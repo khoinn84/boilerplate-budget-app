499c7eb2dc94ef8269b41f91115862ba9edadce3
Replit user <>
2023-12-07 01:49:54 +0000

    push directly from Replit

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ food = budget.Category("Food")
 food.deposit(1000, "initial deposit")
 food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
+
 clothing = budget.Category("Clothing")
+print('This is ',clothing.name, ' ledger \n', clothing.ledger)
 food.transfer(50, clothing)
+print(food.get_balance())
+print('This is ',food.name, ' ledger \n', food.ledger)
 clothing.withdraw(25.55)
 clothing.withdraw(100)
+print(clothing.get_balance())
+
+
 auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
+auto.deposit(2000, "initial deposit")
 auto.withdraw(15)
+print('===========')
+print('This is ',food.name, ' ledger \n', food.ledger)
+print('===========')
+print(clothing.ledger)
+print('===========')
+print(auto.ledger)
 
-print(food)
-print(clothing)
-
-print(create_spend_chart([food, clothing, auto]))
+#print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
